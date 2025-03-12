d5969ea231ffb7b54850d250fddb1776f6b73903
Kulsoom Imran <kulsoomimran1901@gmail.com>
2025-03-12 22:29:21 +0500

    update skills and projects

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,12 +7,18 @@ export default function About() {
             </p>
             <h3 className="text-3xl sm:text-4xl font-bold text-purple-700 mb-6">My Skills</h3>
             <ul className="grid grid-cols-2 sm:grid-cols-3 gap-6 lg:gap-8 mb-12">
+
+                <li className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">HTML</li>
+                <li className="bg-gradient-to-r from-purple-600 via-violet-500 to-blue-500 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">CSS</li>
                 <li className="bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">TypeScript</li>
                 <li className="bg-gradient-to-r from-teal-400 via-green-400 to-blue-400 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">Next.js</li>
                 <li className="bg-gradient-to-r from-yellow-400 via-orange-400 to-red-400 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">Tailwind CSS</li>
                 <li className="bg-gradient-to-r from-green-400 via-blue-500 to-purple-500 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">Node.js</li>
-                <li className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">HTML</li>
-                <li className="bg-gradient-to-r from-purple-600 via-violet-500 to-blue-500 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">CSS</li>
+                <li className="bg-gradient-to-r from-blue-500 via-cyan-500 to-teal-500 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">Python</li>
+                <li className="bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">Streamlit</li>
+                <li className="bg-gradient-to-r from-gray-700 via-gray-500 to-gray-300 text-white px-6 py-4 rounded-lg shadow-xl text-base sm:text-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">UV</li>
+
+                <li></li>
             </ul>
         </section>
     )
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
     const cliProjects = [
         {
             title: 'Simple Calculator',
-            Description: 'A simple command line calculator that allow user to perform simple operations like addition, subtraction, multiplication and division!',
+            Description: 'A simple command line calculator built with TypeScript that allow user to perform simple operations like addition, subtraction, multiplication and division!',
             repoLink: 'https://github.com/kulsoomimran/simple-calculator',
             command: 'npx k1901-simple-calculator'
         },
@@ -19,45 +19,44 @@ const Projects = () => {
             repoLink: 'https://github.com/kulsoomimran/number-guessing',
             command: 'npx ki-1901-number-guessing-game'
         },
-
         {
             title: 'ATM',
-            Description: 'A command-line interface ATM that allows users to perform essential banking operations in a simple and efficient manner!',
+            Description: 'A command-line interface ATM built with TypeScript that allows users to perform essential banking operations in a simple and efficient manner!',
             repoLink: 'https://github.com/kulsoomimran/cli-atm',
             command: 'npx k1901-cli-atm'
         },
 
         {
             title: 'Todo List',
-            Description: 'An interactive, command-line to-do list application that allows users to efficiently manage tasks, with options to view, add, update or delete tasks!',
+            Description: 'An interactive, command-line to-do list application built with TypeScript that allows users to efficiently manage tasks, with options to view, add, update or delete tasks!',
             repoLink: 'https://github.com/kulsoomimran/todos-list',
             command: 'npx k1901-todos-list'
         },
 
         {
             title: 'Currency Converter',
-            Description: 'A command line tool that allows users to effortlessly convert amounts between different currencies!',
+            Description: 'A command line tool built with TypeScript that allows users to effortlessly convert amounts between different currencies!',
             repoLink: 'https://github.com/kulsoomimran/currency-converter',
             command: 'npx k1901-currency-converter'
         },
 
         {
             title: 'Word Counter',
-            Description: 'A command line tool that allows users to effortlessly convert amounts between different currencies!',
+            Description: 'A command line tool built with TypeScript that allows users to effortlessly convert amounts between different currencies!',
             repoLink: 'https://github.com/kulsoomimran/currency-converter',
             command: 'npx k1901-currency-converter'
         },
 
         {
             title: 'Quiz System',
-            Description: 'A command line quiz consist of 5 questions about TypeScript and user have to select the most suitable and correct option!',
+            Description: 'A command line quiz built with TypeScript consist of 5 questions about TypeScript and user have to select the most suitable and correct option!',
             repoLink: 'https://github.com/kulsoomimran/Quiz_System',
             command: 'npx k901-quiz-system'
         },
 
         {
             title: 'Countdown Timer',
-            Description: 'A customizable, command-line countdown timer for easy terminal-based time tracking!',
+            Description: 'A customizable, command-line countdown timer built with TypeScript for easy terminal-based time tracking!',
             repoLink: 'https://github.com/kulsoomimran/countdown-timer',
             command: 'npx kulsoomiimran-countdown-timer'
         },
@@ -65,19 +64,23 @@ const Projects = () => {
 
     const webProjects = [
         {
-            title: 'Static Resume',
-            Description: "My Static Resume, using HTML and CSS",
-            repoLink: 'https://github.com/kulsoomimran/Interactive_Resume_Builder/tree/main/Milestone1',
-            deploymentLink: 'https://static-resume-builder-psi-ruddy.vercel.app/'
+            title: 'Password Generator',
+            Description: 'A simple password generator app built with Python, UV and Streamlit, where user can generate a random password of desired length and complexity!',
+            repoLink: 'https://github.com/kulsoomimran/password-generator.git',
+            deploymentLink: 'https://python-password-generator.streamlit.app/'
         },
-
         {
-            title: 'Dynamic Resume Builder',
-            Description: "A dynamic resume builder,using HTML, CSS, and Typescript(for functionality) where user puts their input and resume genereated. users can directly edit the resume and the changes are reflected immediately without reloading the page. User can also download the resume as a PDF!",
-            repoLink: 'https://github.com/kulsoomimran/Interactive_Resume_Builder/tree/main/Milestone05',
-            deploymentLink: 'https://hackathon1-milestone05.vercel.app/'
+            title: 'Unit Converter',
+            Description: 'A simple unit converter app built with Python, UV and Streamlit, where user can convert units of length, temperature, and weight!!',
+            repoLink: 'https://github.com/kulsoomimran/unit-converter.git',
+            deploymentLink: 'https://unit-converter-by-kulsoomimran.streamlit.app/'
         },
-
+        {
+            title: 'Countdown Timer',
+            Description: " A simple countdown timer application, built with Next.js and Tailwind CSS where user can set a time duration, start, pause, and reset the timer and display the remaining time",
+            repoLink: 'https://github.com/kulsoomimran/CountdownTimer',
+            deploymentLink: 'https://vercel.com/kulsoom-imrans-projects/countdown-timer'
+        },
         {
             title: 'The CHARMLY-A Jewelry Website',
             Description: "A stylish jewelry website, The CHARMLY, built with Next.js and Tailwind CSS, featuring reusable components, smooth routing, and a responsive design!",
@@ -112,13 +115,17 @@ const Projects = () => {
             repoLink: 'https://github.com/kulsoomimran/PixelPerfectPage02',
             deploymentLink: 'https://pixel-perfect-page02.vercel.app/'
         },
-
-        
         {
-            title: 'Countdown Timer',
-            Description: " A simple countdown timer application, built with Next.js and Tailwind CSS where user can set a time duration, start, pause, and reset the timer and display the remaining time",
-            repoLink: 'https://github.com/kulsoomimran/CountdownTimer',
-            deploymentLink: 'https://vercel.com/kulsoom-imrans-projects/countdown-timer'
+            title: 'Dynamic Resume Builder',
+            Description: "A dynamic resume builder,using HTML, CSS, and Typescript(for functionality) where user puts their input and resume genereated. users can directly edit the resume and the changes are reflected immediately without reloading the page. User can also download the resume as a PDF!",
+            repoLink: 'https://github.com/kulsoomimran/Interactive_Resume_Builder/tree/main/Milestone05',
+            deploymentLink: 'https://hackathon1-milestone05.vercel.app/'
+        },
+        {
+            title: 'Static Resume',
+            Description: "My Static Resume, using HTML and CSS",
+            repoLink: 'https://github.com/kulsoomimran/Interactive_Resume_Builder/tree/main/Milestone1',
+            deploymentLink: 'https://static-resume-builder-psi-ruddy.vercel.app/'
         },
 
     ];
